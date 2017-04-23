127dd9662d5f5ac66c5dfd606b6be2bcffb3b522
TGwonton <winston96lan@gmail.com>
2017-04-23 14:33:12 -0700

    Changed to 20

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -19,8 +19,8 @@ class ViewController: UIViewController {
         tapCount = tapCount + 1
         print(tapCount)
         
-        if tapCount >= 10 {
-            theLabel.text = "You tapped the button 10 times!"
+        if tapCount >= 20 {
+            theLabel.text = "You tapped the button 20 times!"
         }
     }
     
