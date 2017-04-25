4efb6053170a30b08419724208a7b61d02b8c06c
TGwonton <winston96lan@gmail.com>
2017-04-24 17:01:54 -0700

    Added subtraction functionality

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -15,7 +15,13 @@ class ViewController: UIViewController {
     @IBOutlet var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
-        theLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        let addition = false
+        
+        if addition {
+            theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
     }
     
     override func viewDidLoad() {
@@ -23,12 +29,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
